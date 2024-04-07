a92e035eda39bdf29b53223c60a0f0cdc36926ed
Alexandre Ferreira <shieldforce2@gmail.com>
2024-04-07 11:02:16 -0300

    console

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::name("api.")/*->middleware('auth:sanctum')*/->group(function () {
     Route::name("command.")->prefix("command")->group(function () {
-        Route::get("/{group?}", [ CommandController::class, "index" ])->name("index");
+        Route::get("/{group?}/{type?}", [ CommandController::class, "index" ])->name("index");
         Route::post("/", [ CommandController::class, "store" ])->name("store");
         Route::put("/{command}", [ CommandController::class, "update" ])->name("update");
         Route::delete("/{command}", [ CommandController::class, "delete" ])->name("delete");

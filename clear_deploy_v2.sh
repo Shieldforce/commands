f9209668bcbc762714009db8842b54cf49a495cc
Alexandre Ferreira <shieldforce2@gmail.com>
2024-05-18 18:35:18 -0300

    login

--- clear_deploy_v2.sh
+++ clear_deploy_v2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-php artisan optimize
-php artisan config:clear
-php artisan migrate --force
-php artisan db:seed --class=PermissionsSeeders --force
-php artisan horizon:install
-php artisan horizon:publish
+docker exec -it laravel-php-fpm-8.2-55 php artisan optimize
+docker exec -it laravel-php-fpm-8.2-55 php artisan config:clear
+docker exec -it laravel-php-fpm-8.2-55 php artisan migrate --force
+docker exec -it laravel-php-fpm-8.2-55 php artisan db:seed --class=PermissionsSeeders --force
+docker exec -it laravel-php-fpm-8.2-55 php artisan horizon:install
+docker exec -it laravel-php-fpm-8.2-55 php artisan horizon:publish

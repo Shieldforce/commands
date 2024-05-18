88dde522e7f1e63a1f24592df7c23a2903f76948
Alexandre Ferreira <shieldforce2@gmail.com>
2024-05-18 19:00:06 -0300

    login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CommandController;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+
+Route::name("api.")->group(function () {
+    Route::name("command.")->prefix("command")->group(function () {
+        Route::get("/{group?}/{type?}", [ CommandController::class, "index" ])->name("index");
+    });
+});
 
 Route::post('/login', [ AuthController::class, "login" ])->name("api.login");
 Route::post('/register', [ AuthController::class, "register" ])->name("api.register");
